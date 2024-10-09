43545b3e82c212ac1cc84fbd910d99cbbb909950
kasragh23 <kasrapr23@gmail.com>
2024-10-09 16:32:42 +0330

    initial.

--- lib/src/pages/add_category/commons/add_category_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:test_exam/src/pages/add_category/controllers/add_category_controller.dart';
-
-class AddCategoryBinding extends Bindings{
-  @override
-  void dependencies() {
-    Get.lazyPut<AddCategoryController>(()=> AddCategoryController());
-  }
-}
